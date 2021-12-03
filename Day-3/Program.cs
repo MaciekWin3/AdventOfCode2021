7b91d2b6a683880afd8bee6895924638a191aa98
maciekwin3 <maciekwin3@gmail.com>
2021-12-03 11:31:56 +0100

    Day 3 first puzzle solution

--- Day-3/Program.cs
+++ Day-3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(FirstPuzzle("input.txt"));
+
+static int FirstPuzzle(string fileName)
+{
+    var data = GetPuzzleInput(fileName);
+    string gammaRate = "";
+    string epsilonRate = "";
+
+    for (int i = 0; i < data[0].Length; i++)
+    {
+        int zerosCounter = 0;
+        int onesCounter = 0;
+        foreach (var binary in data)
+        {
+            if (binary[i] == '0')
+            {
+                zerosCounter++;
+            }
+            if (binary[i] == '1')
+            {
+                onesCounter++;
+            }
+        }
+
+        if (zerosCounter > onesCounter)
+        {
+            gammaRate += '0';
+            epsilonRate += '1';
+        }
+        else
+        {
+            gammaRate += '1';
+            epsilonRate += '0';
+        }
+    }
+
+    return Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+}
+
+static List<string> GetPuzzleInput(string fileName)
+{
+    return File.ReadAllLines(fileName).ToList();
+}
